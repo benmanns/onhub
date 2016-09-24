d8a00792853400fd5ee88d4a408aa299e9f0f3df
Benjamin Manns <benmanns@gmail.com>
2016-09-24 14:08:30 -0400

    Decompress gzipped file content

---
+++ diagnosticreport/json.go
@@ -0,0 +1,49 @@
+package diagnosticreport
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"io/ioutil"
+)
+
+// From compress/gzip/gunzip.go
+const (
+	gzipID1     = 0x1f
+	gzipID2     = 0x8b
+	gzipDeflate = 8
+)
+
+type gzippedString []byte
+
+// MarshalJSON is overridden to decompress the content field if it is gzip
+// compressed.
+func (f File) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Path    string        `json:"path"`
+		Content gzippedString `json:"content"`
+	}{
+		Path:    f.Path,
+		Content: gzippedString(f.Content),
+	})
+}
+
+func (s gzippedString) MarshalJSON() ([]byte, error) {
+	if len(s) < 3 {
+		return json.Marshal(string(s))
+	}
+	if s[0] != gzipID1 || s[1] != gzipID2 || s[2] != gzipDeflate {
+		return json.Marshal(string(s))
+	}
+	msgReader := bytes.NewReader(s)
+	gzipReader, err := gzip.NewReader(msgReader)
+	if err != nil {
+		return nil, err
+	}
+	defer gzipReader.Close()
+	b, err := ioutil.ReadAll(gzipReader)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(string(b))
+}
